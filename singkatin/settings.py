ca0ebed26afc738b38c56e4dde3a8f95b3987e8d
yanuarkholik <yanuar.0974@students.amikom.ac.id>
2021-03-15 21:22:34 +0700

    Initiate Project

--- singkatin/settings.py
+++ singkatin/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_heroku
+
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
