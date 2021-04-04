22bfb205f7eae79edf7ca9f002170287a42a51de
yanuarkholik <yanuar.0974@students.amikom.ac.id>
2021-04-04 19:36:55 +0700

    Adjusting/adding test data

--- mc_tasklist/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.db import models
-
-class Task(models.Model):
-    name = models.TextField()
--- mc_tasklist/urls.py
+++
@@ -1,7 +0,0 @@
-from mc_tasklist import views
-urlpatterns = [
-    # ...
-    path('add', views.add),
-    path('remove', views.remove),
-    path('', views.index),
-]
